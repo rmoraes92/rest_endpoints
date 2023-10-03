4671eb70fc9e409c3c7d68c12d1882051c8f467b
Ramon Moraes <ramonmoraes8080@gmail.com>
2023-10-03 13:29:56 -0300

    setup - updates license and app name

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -29,6 +29,6 @@ from endpoints.exceptions import HttpMethodIsNotSupported
 __version__ = "0.3.1"
 __author__ = "Ramon Moraes"
 __author_email__ = "ramonmoraes8080@gmail.com"
-__license__ = "MIT"
-__app_name__ = "endpoints_mapper"
+__license__ = "GPLv3"
+__app_name__ = "endpoints"
 __description__ = "ReST API Endpoint Mapper"
