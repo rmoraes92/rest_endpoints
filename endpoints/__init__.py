5803937a21f56d07f1dcbb4882ea184d941dbc64
Ramon Moraes <ramonmoraes8080@gmail.com>
2023-10-03 14:08:48 -0300

    bumping version to 0.4.0

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -26,7 +26,7 @@ from endpoints.api import Endpoint
 from endpoints.exceptions import HttpMethodIsNotSupported
 
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __author__ = "Ramon Moraes"
 __author_email__ = "ramonmoraes8080@gmail.com"
 __license__ = "GPLv3"
