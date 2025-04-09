20a28b7ee2db25ca24492487ea38b07614d2184c
Ramon Moraes <ramonmoraes.foss@gmail.com>
2025-04-09 18:28:21 -0300

    tests - adds tests for aiohttp

---
+++ tests_aio.py
@@ -0,0 +1,172 @@
+'''
+The MIT License (MIT)
+
+Copyright © 2024 Ramon Moraes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+
+import unittest
+# from unittest.mock import patch, AsyncMock
+
+import asyncio
+
+from rest_endpoints import GET
+from rest_endpoints import AsyncEndpoint
+from rest_endpoints import Credential
+from rest_endpoints import HttpMethodIsNotSupported
+
+
+class AsyncEndpointTestCase(unittest.IsolatedAsyncioTestCase):
+
+    def test_get_url(self):
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+
+        class FooBarEndpoint(FooEndpoint):
+            path = '/bar'
+
+        bar = FooBarEndpoint()
+        self.assertEqual(bar.get_url(), 'http://foo/bar')
+
+    def test_get_url_appended_slash(self):
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo/'
+
+        class FooBarEndpoint(FooEndpoint):
+            path = '/bar'
+
+        bar = FooBarEndpoint()
+        self.assertEqual(bar.get_url(), 'http://foo/bar')
+
+    def test_get_url_missing_slash(self):
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+
+        class FooBarEndpoint(FooEndpoint):
+            path = 'bar'
+
+        bar = FooBarEndpoint()
+        self.assertEqual(bar.get_url(), 'http://foo/bar')
+
+    def test_get_url_variable(self):
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+
+        class FooBarEndpoint(FooEndpoint):
+            path = 'bar/{bar_id}'
+
+        bar = FooBarEndpoint(bar_id=1)
+        self.assertEqual(bar.get_url(), 'http://foo/bar/1')
+
+    def test_query_params_merging(self):
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+            query_params = {'hello': 1}
+
+        class FooBarEndpoint(FooEndpoint):
+            path = 'bar'
+
+            def get_query_params(self):
+                return super().get_query_params({
+                    'world': 2,
+                    })
+
+        bar = FooBarEndpoint()
+        self.assertDictEqual(bar.get_query_params(), {'hello': 1, 'world': 2})
+
+    def test_headers_merging(self):
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+            headers = {'hello': 1}
+
+        class FooBarEndpoint(FooEndpoint):
+            path = 'bar'
+
+            def get_headers(self):
+                return super().get_headers({
+                    'world': 2,
+                    })
+
+        bar = FooBarEndpoint()
+        self.assertDictEqual(bar.get_headers(), {'hello': 1, 'world': 2})
+
+    async def test_unsupported_methods(self):
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+
+        class FooBarEndpoint(FooEndpoint):
+            path = 'bar'
+            methods = [
+                GET,
+                ]
+
+        c = Credential()
+        with self.assertRaises(HttpMethodIsNotSupported):
+            await FooBarEndpoint(c).post()
+
+    def test_credentials_headers(self):
+        class TokenCredential(Credential):
+            headers = {
+                'Authorization': 'Bearer FooBar',
+                }
+
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+            headers = {
+                'Content-Type': 'application/json',
+                }
+
+        class FooBarEndpoint(FooEndpoint):
+            path = 'bar'
+
+        c = TokenCredential()
+        bar = FooBarEndpoint(c)
+        headers = {
+            'Authorization': 'Bearer FooBar',
+            'Content-Type': 'application/json',
+            }
+        self.assertDictEqual(bar.get_headers(), headers)
+
+    def test_credentials_query_params(self):
+        class TokenCredential(Credential):
+            query_params = {
+                'token': 'FooBar',
+                }
+
+        class FooEndpoint(AsyncEndpoint):
+            domain = 'http://foo'
+            query_params = {
+                'format': 'json',
+                }
+
+        class FooBarEndpoint(FooEndpoint):
+            path = 'bar'
+
+        c = TokenCredential()
+        bar = FooBarEndpoint(c)
+        query_params = {
+            'token': 'FooBar',
+            'format': 'json',
+            }
+        self.assertDictEqual(bar.get_query_params(), query_params)
+
+if __name__ == '__main__':
+    unittest.main()
